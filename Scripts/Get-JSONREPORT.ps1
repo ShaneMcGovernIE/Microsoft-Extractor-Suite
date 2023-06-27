503e1c1ef81bd8b842635d0d113907b293274db1
Shane McGovern <smcgovernie@icloud.com>
2023-06-27 16:27:57 +0000

    Jun 27, 2023, 5:27 PM

---
+++ Scripts/Get-JSONREPORT.ps1
@@ -0,0 +1,3 @@
+
+
+$JSON | Select-Object -Property @{Name = 'Created'; Expression = { $_.CreatedDateTime } }, @{Name = 'Output'; Expression = { $_.Status.AdditionalDetails } }, @{Name = 'Name'; Expression = { $_.UserDisplayName } }, @{Name = 'Email'; Expression = { $_.UserPrincipalName } }, @{Name = 'Location'; Expression = { $_.Location.State } }, @{Name = 'Status'; Expression = { $_.ConditionalAccessStatus } }, @{Name = 'Application'; Expression = { $_.AppDisplayName } }, IpAddress, @{Name = 'Device'; Expression = { $_.ClientAppUsed } } | Out-HtmlView
